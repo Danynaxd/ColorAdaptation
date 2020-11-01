816e46de91a22161660d2480d80a8ccde928449f
Danynaxd <30656300+Danynaxd@users.noreply.github.com>
2020-11-01 23:40:34 +0300

    Commit project

---
+++ ColorAdaptation/Assets/MainFolder/1/Scripts/Cube_rotation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube_rotation : MonoBehaviour {
+	public int speed, tilt;
+	public Vector3 target;
+
+	void Update() {
+		transform.Rotate (Vector3.up * tilt); 
+		transform.Rotate (Vector3.left * speed);
+	
+	}
+
+}
---
+++ ColorAdaptation/Assets/MainFolder/1/Scripts/NextLevel.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class NextLevel : MonoBehaviour {
+
+	public GameObject m_on, m_off;
+	public void Play(){
+		SceneManager.LoadScene ("play");
+	}
+	public void MainScene(){
+		SceneManager.LoadScene ("Main");
+
+	}
+	public void Help(){
+		SceneManager.LoadScene ("Help");
+	}
+	public void About(){
+		SceneManager.LoadScene ("About");
+	}
+
+	void OnMouseAsButton() {	
+	
+		switch (gameObject.name) {
+		case "Music":
+			if (PlayerPrefs.GetString ("Music") != "no") {
+				PlayerPrefs.SetString ("Music", "no");
+				m_on.SetActive (false);
+				m_off.SetActive (true);
+			} else 
+			{
+				PlayerPrefs.SetString ("Music", "yes");
+				m_on.SetActive (true);
+				m_off.SetActive (false);
+			}
+			break;
+		}
+	}
+}
